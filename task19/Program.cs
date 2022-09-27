9273323107774a3bb5ad263ef144a7c4ecd83b7e
Aleksei <spacerabbit1@mail.ru>
2022-09-27 19:09:39 +0700

    end

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int x_tmp, x2 = 0; x_tmp = x;       
+while (x_tmp > 0)
+    {
+     x2 *= 10;
+     x2 += x_tmp % 10;
+     x_tmp /= 10;
+    }
+if (x == x2) 
+    {
+        Console.WriteLine("палиндром");
+    }
+else
+    {
+        Console.WriteLine("не палиндром");
+    }
+ 
